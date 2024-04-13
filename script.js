552d769a1ae96bb192452a358bb826e6228506ab
Unaiz Siddiqui <unaiz4321@gmail.com>
2024-04-13 10:06:17 +0530

    added : payment gateway

--- script.js
+++ script.js
@@ -144,5 +144,9 @@ const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
 
 productButton.addEventListener("click", () => {
-  payment.style.display  = "flex";
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
 });
